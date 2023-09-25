e8a784e0e1f17c64e29be09e476593480336a24e
Suprabhatgit <Ayxxnyrr@gmail.com>
2023-09-25 22:33:47 +0530

    App built

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig={
+//   reactStrictMode: true,
+//   images:{
+//     domains:["links.papareact.com","fakestoreapi.com"]
+//   }
+// }
+// const { withSuperjson } = require('next-superjson')
+// module.exports = withSuperjson()(nextConfig)
+module.exports={
+  images:{
+    domains:["links.papareact.com","fakestoreapi.com"]
+      }
+}
